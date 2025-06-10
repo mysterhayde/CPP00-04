956245a67b673d43bc047916623ae296eb30eba2
mysterhayde <43919667+mysterhayde@users.noreply.github.com>
2025-06-10 11:48:29 +0200

    MakeDeposit and MakeWithwral function

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 18:56:20 by hdougoud          #+#    #+#             */
-/*   Updated: 2025/06/09 22:56:53 by hdougoud         ###   ########.fr       */
+/*   Updated: 2025/06/09 23:48:41 by hdougoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,13 @@
 
 Account::Account(int initial_deposit)
 {
+	this->_accountIndex = _nbAccounts;
+	this->_amount = initial_deposit;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	_nbAccounts++;
+	_totalAmount += initial_deposit;
+	_displayTimestamp();
 	std::cout << "\x1b[32;1m" << "Constructor called" << "\033[0m" << std::endl;
 	return;
 }
@@ -25,24 +32,54 @@ Account::~Account()
 	return;
 }
 
+void	Account::makeDeposit(int deposit)
+{
+	this->_amount += deposit;
+	this->_totalNbDeposits += 1;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	if (this->_amount >= withdrawal)
+	{
+		this->_amount -= withdrawal;
+		return (true);
+	}
+	return (false);
+}
+
+int	Account::checkAmount(void) const
+{
+	return Account::_amount;
+}
+
+void	Account::displayStatus(void) const
+{
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "ammount" << this->_amount << ";";
+	std::cout << "deposit:" << this->_nbDeposits << ";";
+	std::cout << "withdrawals:" << this->_nbWithdrawals << std::endl;
+}
+
 int	Account::getNbAccounts(void)
 {
-	return Account::_nbAccounts;
+	return _nbAccounts;
 }
 
 int	Account::getTotalAmount(void)
 {
-	return Account::_totalAmount;
+	return _totalAmount;
 }
 
 int	Account::getNbDeposits(void)
 {
-	return Account::_totalNbDeposits;
+	return _totalNbDeposits;
 }
 
 int	Account::getNbWithdrawals(void)
 {
-	return Account::_totalNbWithdrawals;
+	return _totalNbWithdrawals;
 }
 
 void	Account::displayAccountsInfos(void)
