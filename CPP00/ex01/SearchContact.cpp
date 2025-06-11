7e7422d5cb5b09ea4031dc764bcbfe5c840628d2
Hayden Dougoud <hdougoud@c1r9s4.42lausanne.ch>
2025-06-11 13:15:33 +0200

    make prettyer

---
+++ CPP00/ex01/SearchContact.cpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   SearchContact.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 13:03:05 by hdougoud          #+#    #+#             */
+/*   Updated: 2025/06/11 13:03:31 by hdougoud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Contact.hpp"
+#include "PhoneBook.hpp"
+
+static int convert_entry(PhoneBook *PhoneBookInstance)
+{
+	int	selection;
+	std::string	entry;
+
+	std::cout << "Select the contact you want to visualize" << std::endl;
+	std::getline(std::cin, entry);
+	try
+	{
+		selection = (std::stoi(entry) - 1);
+		if (selection < 0 || selection > 7 || !PhoneBookInstance->contact[selection].conigured)
+		{
+			std::cout << "Invalid input" << std::endl;
+			return (-1);
+		}
+	}
+	catch(std::logic_error)
+	{
+		std::cout << "Invalid argument " << entry << " is invalid entry" << std::endl;
+		return (-1);
+	}
+	return (selection);
+}
+
+static std::string	print_preview(std::string str)
+{
+	int			size;
+
+	size = str.length();
+	if (size > 10)
+	{
+		str.resize(9);
+		str.push_back('.');
+	}
+	else
+		str.resize(10, ' ');
+	return (str);
+}
+
+void	search_contact(PhoneBook *PhoneBookInstance)
+{
+	for (int i = 0; PhoneBookInstance->contact[i].conigured && i <= 7; i++)
+	{
+		std::cout << i + 1 << "         " << '|'
+		<< print_preview(PhoneBookInstance->contact[i].frist_name) << '|'
+		<< print_preview(PhoneBookInstance->contact[i].last_name) << '|'
+		<< print_preview(PhoneBookInstance->contact[i].nickname) << std::endl;
+	}
+
+	int selection = convert_entry(PhoneBookInstance);
+	if (selection == -1)
+		return ;
+	
+	std::cout << std::endl;
+	std::cout << PhoneBookInstance->contact[selection].frist_name << std::endl
+		<< PhoneBookInstance->contact[selection].last_name << std::endl
+		<< PhoneBookInstance->contact[selection].nickname << std::endl
+		<< PhoneBookInstance->contact[selection].phone_number << std::endl
+		<< PhoneBookInstance->contact[selection].darkest_secret << std::endl << std::endl;
+}
--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hdougoud <hdougoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 20:51:56 by hdougoud          #+#    #+#             */
-/*   Updated: 2025/06/11 12:54:57 by hdougoud         ###   ########.fr       */
+/*   Updated: 2025/06/11 13:03:38 by hdougoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,66 +14,7 @@
 #include "Contact.hpp"
 #include "PhoneBook.hpp"
 
-static int convert_entry(PhoneBook *PhoneBookInstance)
-{
-	int	selection;
-	std::string	entry;
-
-	std::cout << "Select the contact you want to visualize" << std::endl;
-	std::getline(std::cin, entry);
-	try
-	{
-		selection = (std::stoi(entry) - 1);
-		if (selection < 0 || selection > 7 || !PhoneBookInstance->contact[selection].conigured)
-		{
-			std::cout << "Invalid input" << std::endl;
-			return (-1);
-		}
-	}
-	catch(std::logic_error)
-	{
-		std::cout << "Invalid argument " << entry << " is invalid entry" << std::endl;
-		return (-1);
-	}
-	return (selection);
-}
-
-static std::string	print_preview(std::string str)
-{
-	int			size;
-
-	size = str.length();
-	if (size > 10)
-	{
-		str.resize(9);
-		str.push_back('.');
-	}
-	else
-		str.resize(10, ' ');
-	return (str);
-}
-
-static void	search_contact(PhoneBook *PhoneBookInstance)
-{
-	for (int i = 0; PhoneBookInstance->contact[i].conigured && i <= 7; i++)
-	{
-		std::cout << i + 1 << "         " << '|'
-		<< print_preview(PhoneBookInstance->contact[i].frist_name) << '|'
-		<< print_preview(PhoneBookInstance->contact[i].last_name) << '|'
-		<< print_preview(PhoneBookInstance->contact[i].nickname) << std::endl;
-	}
-
-	int selection = convert_entry(PhoneBookInstance);
-	if (selection == -1)
-		return ;
-	
-	std::cout << std::endl;
-	std::cout << PhoneBookInstance->contact[selection].frist_name << std::endl
-		<< PhoneBookInstance->contact[selection].last_name << std::endl
-		<< PhoneBookInstance->contact[selection].nickname << std::endl
-		<< PhoneBookInstance->contact[selection].phone_number << std::endl
-		<< PhoneBookInstance->contact[selection].darkest_secret << std::endl << std::endl;
-}
+void	search_contact(PhoneBook *PhoneBookInstance);
 
 static std::string get_contact_info(std::string contact_info)
 {
@@ -82,7 +23,7 @@ static std::string get_contact_info(std::string contact_info)
 	std::getline(std::cin, entry);
 	while(entry.empty())
 	{
-		std::cout << "\x1b[33;1m" << "The entry can't be empty" << std::endl;
+		std::cout << "\x1b[38;5;208m""The entry can't be empty""\033[0m" << std::endl;
 		std::cout << std::endl << contact_info << std::endl;
 		std::getline(std::cin, entry);
 	}
